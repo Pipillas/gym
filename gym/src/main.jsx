f199719d23296894d71585a566b87653010bcaa2
Pipas <masdamianr@gmail.com>
2024-06-06 19:12:55 -0300

    cambios

--- gym/src/main.jsx
+++ gym/src/main.jsx
@@ -5,7 +5,7 @@ import { io } from 'socket.io-client';
 import Router from './Router.jsx';
 import './styles/all.css';
 
-export const URL = `http://localhost:3000`;
+export const URL = `http://192.168.0.100:3000`;
 export const socket = io(URL);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
